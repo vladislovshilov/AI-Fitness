a28b88033c0a75734ddb40741c83e4302018abf7
Vladislav Shilov <vladyslav.shylov@brainbeanapps.com>
2020-05-16 02:58:32 +0300

    init project

---
+++ AI Fitness/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  AI Fitness
+//
+//  Created by Vlados iOS on 5/16/20.
+//  Copyright © 2020 Vladislav Shilov. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
